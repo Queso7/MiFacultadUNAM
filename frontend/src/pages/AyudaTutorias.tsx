e520cac9ddc531aef60f52ae80ed625b2f37e817
Queso7 <luzazul033@gmail.com>
2025-04-14 18:25:55 -0600

    xd

---
+++ frontend/src/pages/AyudaTutorias.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const AyudaTutorias: React.FC = () => {
+    return (
+        <main className="ayuda-tutorias-page">
+            esotilin
+        </main>
+    );
+};
+
+export default AyudaTutorias;
