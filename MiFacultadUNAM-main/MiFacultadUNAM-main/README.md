6143da70f313ea57c33d7fac255e6c0b8e221f04
DiegoYTP <dxjarcor@gmail.com>
2025-04-17 22:24:59 -0600

    Delete MiFacultadUNAM-main/MiFacultadUNAM-main directory

--- MiFacultadUNAM-main/MiFacultadUNAM-main/README.md
+++
@@ -1,2 +0,0 @@
-# MiFacultadUNAM
-Red social para comunidad UNAM
--- MiFacultadUNAM-main/MiFacultadUNAM-main/frontend/src/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://tu-servidor:5000/api'; // Cambia según tu IP o dominio
-
-export const fetchData = async () => {
-  const response = await axios.get(`${API_URL}/data`);
-  return response.data;
-};
